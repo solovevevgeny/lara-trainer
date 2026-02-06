04e74e7e1ad861a212ff0e085c00804f52b68584
Solovev Evgeny <evgeny.solovev@gmail.com>
2026-02-06 14:31:04 +0300

    user model (migration)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->boolean('is_admin')->default(false);
+            $table->boolean('is_trainer')->default(false);
+            $table->boolean('is_athlete')->default(false);
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->unique();
+
+            // $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
